94cf740e803fad47f6a228dcf50bc126d8d0a03e
Dimitri Brukakis <dimitri@brukakis.net>
2023-07-06 06:09:33 +0200

    Initial Commit

---
+++ ChordInTheAct/ChordInTheActApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordInTheActApp.swift
+//  ChordInTheAct
+//
+//  Created by Dimitri Brukakis on 06.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ChordInTheActApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
